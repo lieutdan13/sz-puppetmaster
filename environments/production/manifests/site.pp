822d6d9fe0e060fc7cc069ea89a685819fb92cfd
Dan Schaefer <dan@schaeferzone.net>
2013-03-25 21:57:04 -0400

    Correcting a syntax error in thegit pull cron

--- environments/production/manifests/site.pp
+++ environments/production/manifests/site.pp
@@ -12,7 +12,7 @@ class test_class {
 node big-bang {
         include test_class
 	cron { pull_puppet:
-		command => "cd /etc/puppet && /usr/bin/git -q pull",
+		command => "cd /etc/puppet && /usr/bin/git pull -q",
 		user    => root,
 		hour    => '*',
 		minute  => '*/2',
