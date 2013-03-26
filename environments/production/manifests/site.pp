bab53f6a3af7f92788aae38d5e6253c82cdd96ba
Dan Schaefer <dan@schaeferzone.net>
2013-03-25 22:17:33 -0400

    Moving puppetmaster related stuff to a module

--- environments/production/manifests/site.pp
+++ environments/production/manifests/site.pp
@@ -11,41 +11,5 @@ class test_class {
 
 node big-bang {
         include test_class
-	cron { pull_puppet:
-		command => "cd /etc/puppet && /usr/bin/git pull > /dev/null",
-		user    => root,
-		hour    => '*',
-		minute  => '*/2',
-		ensure  => present
-	}
-
-	service { "puppetmaster":
-		ensure  => "running",
-		enable  => "true",
-		require => Package["puppetmaster"],
-	}
-
-	file { "/etc/default/puppetmaster":
-		notify  => Service["puppetmaster"],
-		mode    => 644,
-		owner   => "root",
-		group   => "root",
-		require => Package["puppetmaster"],
-		content => "/etc/puppet/files/big-bang/etc_default_puppetmaster",
-	}
-
-	service { "puppet":
-		ensure  => "running",
-		enable  => "true",
-		require => Package["puppet"],
-	}
-
-	file { "/etc/default/puppet":
-		notify  => Service["puppet"],
-		mode    => 644,
-		owner   => "root",
-		group   => "root",
-		require => Package["puppet"],
-		content => "/etc/puppet/files/big-bang/etc_default_puppet",
-	}
+	include puppetmaster
 }
