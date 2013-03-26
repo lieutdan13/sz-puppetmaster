107f8c913cafd4d1145650daf78b01f0d6983566
Dan Schaefer <dan@schaeferzone.net>
2013-03-25 21:48:29 -0400

    Adding puppet daemon log configs to /etc/default

--- environments/production/manifests/site.pp
+++ environments/production/manifests/site.pp
@@ -18,4 +18,34 @@ node big-bang {
 		minute  => '*/2',
 		ensure  => present
 	}
+
+	service { "puppetmaster":
+		ensure  => "running",
+		enable  => "true",
+		require => Package["puppetmaster"],
+	}
+
+	file { "/etc/default/puppetmaster":
+		notify  => Service["puppetmaster"],
+		mode    => 644,
+		owner   => "root",
+		group   => "root",
+		require => Package["puppetmaster"],
+		content => "/etc/puppet/files/big-bang/etc_default_puppetmaster",
+	}
+
+	service { "puppet":
+		ensure  => "running",
+		enable  => "true",
+		require => Package["puppet"],
+	}
+
+	file { "/etc/default/puppet":
+		notify  => Service["puppet"],
+		mode    => 644,
+		owner   => "root",
+		group   => "root",
+		require => Package["puppet"],
+		content => "/etc/puppet/files/big-bang/etc_default_puppet",
+	}
 }
