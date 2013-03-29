1a022f5a61b69d043cba7c9a115a7bb09065a3e2
Dan Schaefer <dan@schaeferzone.net>
2013-03-28 22:25:01 -0400

    Adding internal hosts to DNS

--- environments/production/modules/sz-dns/manifests/init.pp
+++ environments/production/modules/sz-dns/manifests/init.pp
@@ -17,13 +17,33 @@ class sz-dns inherits dns::server {
 		soa         => 'ns1.schaeferzone.net',
 		soa_email   => 'dan.schaeferzone.net',
 		nameservers => ['ns1'],
-		serial      => 2013032800
+		serial      => 2013032801
 	}
 
 	dns::zone { '10.168.192.IN-ADDR.ARPA':
 		soa         => 'ns1.schaeferzone.net',
 		soa_email   => 'dan.schaeferzone.net',
 		nameservers => ['ns1'],
-		serial      => 2013032800
+		serial      => 2013032801
+	}
+
+	# A Records
+	dns::record::a {
+		'big-bang':
+			zone => 'schaeferzone.net',
+			data => ['192.168.10.5'],
+			ptr  => true;
+		'raspberrypi':
+			zone => 'schaeferzone.net',
+			data => ['192.168.10.10'],
+			ptr  => true;
+		'nebula':
+			zone => 'schaeferzone.net',
+			data => ['192.168.10.15'],
+			ptr  => true;
+		'eclipse':
+			zone => 'schaeferzone.net',
+			data => ['192.168.10.25'],
+			ptr  => true;
 	}
 }
