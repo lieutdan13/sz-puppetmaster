b657e83f53fa8a303d058da6bb67d019d54cac33
Dan Schaefer <dan@schaeferzone.net>
2013-04-04 20:05:53 -0400

    Moving node definitions to a nodes.pp file

---
+++ manifests/nodes.pp
@@ -0,0 +1,28 @@
+node default {
+	include test_class
+	include utility
+	include puppetagent
+
+	host { "big-bang.schaeferzone.net":
+		comment    => "Just in case DNS is down, we want to know how to get to the Master",
+		ensure     => present,
+		host_aliases => [ 'big-bang' ],
+		ip         => '192.168.10.5',
+		name       => 'big-bang.schaeferzone.net',
+	}
+
+	class { "ntp":
+	        ensure     => running,
+	        servers    => [ 'time1.google.com', 'time2.google.com', 'time3.google.com', 'time4.google.com' ],
+		autoupdate => true,
+	}
+}
+
+node big-bang inherits default {
+	include puppetmaster
+	include sz-dns::client
+}
+
+node raspberrypi inherits default {
+	include sz-dns::server
+}
