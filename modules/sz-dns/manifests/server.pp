d33b2e4d248926c7a41eb781e40d58ffb5477b23
Dan Schaefer <dan@schaeferzone.net>
2013-08-07 21:25:55 -0400

    Adding a eternallands site to raspberrypi along with DNS

--- modules/sz-dns/manifests/server.pp
+++ modules/sz-dns/manifests/server.pp
@@ -26,7 +26,7 @@ class sz-dns::server inherits dns::server {
 		soa         => 'ns1.schaeferzone.net',
 		soa_email   => 'dan.schaeferzone.net',
 		nameservers => ['ns1'],
-		serial      => 2013080602
+		serial      => 2013080700
 	}
 
 	dns::zone { '10.168.192.IN-ADDR.ARPA':
@@ -76,6 +76,9 @@ class sz-dns::server inherits dns::server {
 			zone => $sz_zone,
 			data => 'eclipse.schaeferzone.net';
 		'cars':
+			zone => $sz_zone,
+			data => 'raspberrypi.schaeferzone.net';
+		'eternallands':
 			zone => $sz_zone,
 			data => 'raspberrypi.schaeferzone.net';
 		'favorites':
