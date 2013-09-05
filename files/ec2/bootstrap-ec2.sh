2fec0912f9a545d649beb6c0fd818bb96642c7eb
Dan Schaefer <dan@schaeferzone.net>
2013-09-04 23:57:03 -0400

    Adding a bootstrap script specifically for EC2 instances

---
+++ files/ec2/bootstrap-ec2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+AUTO_HOSTNAME=/usr/sbin/auto_hostname.sh
+wget -O $AUTO_HOSTNAME https://raw.github.com/lieutdan13/sz-puppetmaster/master/files/ec2/auto_hostname.sh
+chmod o+x $AUTO_HOSTNAME
+
+sed -i 's@^exit 0@'${AUTO_HOSTNAME}'\nexit 0@' /etc/rc.local
+
+echo "The bootstrap has been configured. Now reboot and test the hostname when the machine comes back up"
