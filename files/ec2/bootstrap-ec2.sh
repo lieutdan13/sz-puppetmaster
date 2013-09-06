5333a2f75011e74a1a6315aff26aab6eda6b0661
Dan Schaefer <dan@schaeferzone.net>
2013-09-05 21:22:12 -0400

    Adding a running of auto_hostname.sh during ec2 boostrapping

--- files/ec2/bootstrap-ec2.sh
+++ files/ec2/bootstrap-ec2.sh
@@ -6,4 +6,6 @@ chmod u+x $AUTO_HOSTNAME
 
 sed -i 's@^exit 0@'${AUTO_HOSTNAME}'\nexit 0@' /etc/rc.local
 
+$AUTO_HOSTNAME
+
 echo "The bootstrap has been configured. Now reboot and test the hostname when the machine comes back up"
