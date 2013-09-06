3301c01857fd316b5a9b5a7ca82f1abdc4a49be2
Dan Schaefer <dan@schaeferzone.net>
2013-09-05 21:50:03 -0400

    Adding a puppet run during ec2 bootstrap

--- files/ec2/bootstrap-ec2.sh
+++ files/ec2/bootstrap-ec2.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "`whoami`" != "root" ]; then
+	echo "You must be root"
+	exit 1
+fi
+
 AUTO_HOSTNAME=/usr/sbin/auto_hostname.sh
 wget -O $AUTO_HOSTNAME https://raw.github.com/lieutdan13/sz-puppetmaster/master/files/ec2/auto_hostname.sh
 chmod u+x $AUTO_HOSTNAME
@@ -7,5 +12,11 @@ chmod u+x $AUTO_HOSTNAME
 sed -i 's@^exit 0@'${AUTO_HOSTNAME}'\nexit 0@' /etc/rc.local
 
 $AUTO_HOSTNAME
+
+puppet agent --server=puppet.schaeferzone.net -t
+echo "Run the following command on the puppet master"; echo
+echo "sudo puppet cert sign $HOSTNAME"; echo
+echo "Press [ENTER] to continue"
+puppet agent --server=puppet.schaeferzone.net -t
 
 echo "The bootstrap has been configured. Now reboot and test the hostname when the machine comes back up"
