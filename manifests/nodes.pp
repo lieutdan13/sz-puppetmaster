672ff368f8323762ef1c53139285c83fc7a25c15
Dan Schaefer <dan@schaeferzone.net>
2014-02-23 23:32:34 -0500

    Changing A-Web-1's wordpress multisite setting to migrate

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -78,7 +78,7 @@ node a-web-1 inherits puppetagent {
             db_type     => 'remote_mysql',
             db_user     => 'wrdprss',
             multidb     => false,
-            multisite   => 'allow',
+            multisite   => 'migrate',
             options     => {
                 main_site => 'blogs.schaeferzone.net',
                 plugins   => {
