ad17325d95a876f2ae5f2d7d62bd059c4db164c1
Dan Schaefer <dan@schaeferzone.net>
2014-06-02 23:43:03 -0400

    Adding a versioned responsive theme

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -115,6 +115,7 @@ node a-web-1 inherits puppetagent {
                     'wordpress-importer' => '0.6.1',
                 },
                 themes    => {
+                    'responsive' => '1.9.6.1',
                     'something-fishy' => '1.10',
                     'twentytwelve' => '1.3',
                 },
